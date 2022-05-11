7684034ee58e057c84005d7d9b39334d81f59470
Eduardo Retamales <eduardoretamalesrojas@gmail.com>
2022-05-11 19:59:42 -0400

    update site metabonding

---
+++ view/navbar.php
@@ -0,0 +1,64 @@
+<!--RD Navbar-->
+<div class="rd-navbar-wrap">
+          <nav class="rd-navbar rd-navbar-classic" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-static" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-lg-stick-up-offset="46px" data-xl-stick-up-offset="46px" data-xxl-stick-up-offset="46px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
+            <div class="rd-navbar-collapse-toggle rd-navbar-fixed-element-1" data-rd-navbar-toggle=".rd-navbar-collapse"><span></span></div>
+            <div class="rd-navbar-aside-outer rd-navbar-collapse bg-gray-dark">
+              <div class="rd-navbar-aside">
+                <ul class="list-inline navbar-contact-list">
+                  <li>
+                    <div class="unit unit-spacing-xs align-items-center">
+                      <div class="unit-left"><span class="icon text-middle fa-phone"></span></div>
+                      <div class="unit-body"><a href="tel:#">+569 3023 5938</a></div>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="unit unit-spacing-xs align-items-center">
+                      <div class="unit-left"><span class="icon text-middle fa-envelope"></span></div>
+                      <div class="unit-body"><a href="mailto:#">cristian@metalbonding.com</a></div>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="unit unit-spacing-xs align-items-center">
+                      <div class="unit-left"><span class="icon text-middle fa-map-marker"></span></div>
+                      <div class="unit-body"><a href="#">juan fernandez 5253b</a></div>
+                    </div>
+                  </li>
+                </ul>
+                <ul class="social-links">
+                  <li><a class="icon icon-sm icon-circle icon-circle-md icon-bg-white fa-linkedin" href="#"></a></li>
+                  <li><a class="icon icon-sm icon-circle icon-circle-md icon-bg-white fa-twitter" href="#"></a></li>
+                  <li><a class="icon icon-sm icon-circle icon-circle-md icon-bg-white fa-facebook" href="#"></a></li>
+                  <li><a class="icon icon-sm icon-circle icon-circle-md icon-bg-white fa-instagram" href="#"></a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="rd-navbar-main-outer">
+              <div class="rd-navbar-main">
+                <!--RD Navbar Panel-->
+                <div class="rd-navbar-panel">
+                  <!--RD Navbar Toggle-->
+                  <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                  <!--RD Navbar Brand-->
+                  <div class="rd-navbar-brand">
+                    <!--Brand <a class="brand" href="index.html"><img class="brand-logo-dark" src="#" alt="" width="100" height="17"/><img class="brand-logo-light" src="images/logo-inverse-200x34.png" alt="" width="100" height="17"/></a> -->
+                    <h1 id="texto-logo">metalbonding</h1>
+                  </div>
+                </div>
+                <div class="rd-navbar-main-element">
+                  <div class="rd-navbar-nav-wrap">
+                    <ul class="rd-navbar-nav">
+                      <li class="rd-nav-item active"><a class="rd-nav-link" href="index.php">Home</a>
+                      </li>
+                      <li class="rd-nav-item"><a class="rd-nav-link" href="sobrenosotros.php">Sobre nosotros</a>
+                      </li>
+                      <li class="rd-nav-item"><a class="rd-nav-link" href="cotizador.php">Cotizador</a>
+                      </li>
+                      <li class="rd-nav-item"><a class="rd-nav-link" href="contacto.php">Contacto</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
---
+++ view/servicios.php
@@ -0,0 +1,45 @@
+ <!--Owl Carousel-->
+ <section class="section section-md bg-gray-lighten">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-xl-10">
+              <div class="section-name wow fadeInRight text-center" data-wow-delay=".2s">testimonials</div>
+              <h3 class="wow fadeInLeft text-capitalize text-center" data-wow-delay=".3s">Nuestros<span class="text-primary"> servicios</span></h3>
+              <div class="owl-carousel review-carousel" data-items="1" data-sm-items="1" data-md-items="1" data-lg-items="1" data-xl-items="1" data-xxl-items="1" data-dots="true" data-nav="false" data-stage-padding="0" data-loop="false" data-margin="0" data-mouse-drag="true" data-autoplay="false">
+                <div class="item">
+                  <div class="item-preview wow fadeInDown" data-wow-delay=".2s"><img src="img/logo.svg" alt="" width="216" height="108"/>
+                  </div>
+                  <div class="item-description wow fadeInUp" data-wow-delay=".3s">
+                    <p>I was impressed by the quality of what your specialists did. They quickly found out what was the main problem of my company and helped me form a correct strategy, which worked perfectly and set my business on the right path.</p>
+                    <div class="item-subsection"><span class="item-subsection-title devider-left">Servicio 1</span><span>Regular Client</span></div>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="item-preview wow fadeInDown" data-wow-delay=".2s"><img src="img/logo.svg" alt="" width="216" height="108"/>
+                  </div>
+                  <div class="item-description wow fadeInUp" data-wow-delay=".3s">
+                    <p>I appreciate what you’ve done to help my small company develop and thrive in that market niche that we specialize in. Your business consulting experts are the best in the whole industry and I’m going to recommend you to my partners!</p>
+                    <div class="item-subsection"><span class="item-subsection-title devider-left">Servicio 2</span><span>Regular Client</span></div>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="item-preview wow fadeInDown" data-wow-delay=".2s"><img src="img/logo.svg" alt="" width="216" height="108"/>
+                  </div>
+                  <div class="item-description wow fadeInUp" data-wow-delay=".3s">
+                    <p>My cooperation with your team helped me understand that you really care about your clients – it was proved by a lot of positive reviews already – and that's why I chose you. Thank you for a great job, you are true professionals!</p>
+                    <div class="item-subsection"><span class="item-subsection-title devider-left">Servicio 3</span><span>Regular Client</span></div>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="item-preview wow fadeInDown" data-wow-delay=".2s"><img src="img/logo.svg" alt="" width="216" height="108"/>
+                  </div>
+                  <div class="item-description wow fadeInUp" data-wow-delay=".3s">
+                    <p>Two years after СonsultBiz’s involvement, we are remarkably successful and the leader in served markets. This brought industry knowledge, best practices, and real-world solutions to the intricate challenges we faced.</p>
+                    <div class="item-subsection"><span class="item-subsection-title devider-left">Servicio 4</span><span>Regular Client</span></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
